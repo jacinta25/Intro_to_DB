94ddc7f71a4cc47418de73256c9d7dfb74ab1cd0
jacinta25 <mwendejacinta25@gmail.com>
2024-10-23 11:17:51 +0300

    Populating Your Database with Your More Data

--- task_6.sql
+++ task_6.sql
@@ -1,4 +1,4 @@
-USE alx_book_store
+USE alx_book_store;
 INSERT INTO customer(customer_id, customer_name, email, address)
 VALUES (2, 'Blessing malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Hapiness ave.'),
