b03cdcc5a014531916a2c91bce822a78da692e7e
jacinta25 <mwendejacinta25@gmail.com>
2024-10-23 09:24:09 +0300

    Let's Build Your Database: Your Gateway to Data Adventure!

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,28 @@
 #connect to mysql server
 import mysql.connector
+from mysql.connector import Error
 
-mydb = mysql.connector.connect(
-    host = "localhost",
-    user = "jacinta25",
-    password = "condition2030.KE",
-    database = "alx_book_store"
-)
+try:
+    mydb = mysql.connector.connect(
+        host = "localhost",
+        user = "jacinta25",
+        password = "condition2030.KE",
+        database = "alx_book_store"
+    )
 #create a new database
-mycursor = mydb.cursor()
-mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+    print("Database 'alx_book_store' created successfully!")
+
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
 
 
-
-
-print("Database 'alx_book_store' created successfully!")
-
-
-mycursor.close()
-mydb.close()
+finally:
+    
+    if 'mycursor' in locals() and mycursor: 
+        mycursor.close()
+    if 'mydb' in locals() and mydb.is_connected():
+        mydb.close()
+        print("Mysql connection is closed")
